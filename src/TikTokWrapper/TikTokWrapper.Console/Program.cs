8b390a142685f032d0b9957a42e2a29dd4ca154f
Szymon Smykała <ctk7000@gmail.com>
2021-03-23 19:26:23 +0100

    Addfactory (#11)
    * added factory
    
    * refactored

--- src/TikTokWrapper/TikTokWrapper.Console/Program.cs
+++ src/TikTokWrapper/TikTokWrapper.Console/Program.cs
@@ -1,11 +1,15 @@
-﻿namespace TikTokWrapper.WindowsConsole
+﻿using TikTokWrapper.Core;
+using TikTokWrapper.Core.Factories;
+
+namespace TikTokWrapper.WindowsConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-           
+           ITikTokManagerFactory factory = new TikTokManagerFactory();
+           var manager = factory.Create(@"C:\Users\Szymon\AppData\Local\Programs\Python\Python39\python.exe");
+           var trending = manager.GetTrending();
         }
     }
 }
---
+++ src/TikTokWrapper/TikTokWrapper.Core/Factories/TikTokManagerFactory.cs
@@ -0,0 +1,14 @@
+﻿using TikTokWrapper.Core.Internal;
+using TikTokWrapper.Core.Internal.Utilities;
+
+namespace TikTokWrapper.Core.Factories
+{
+    public class TikTokManagerFactory : ITikTokManagerFactory
+    {
+        public ITikTokManager Create(string pythonExePath)
+        {
+            var pythonRunner = new PythonRunner(pythonExePath);
+            return new TikTokManager(pythonRunner);
+        }
+    }
+}
