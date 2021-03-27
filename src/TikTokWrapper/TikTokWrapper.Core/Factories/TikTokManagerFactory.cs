4530253797f6b72129b658cf5a16e9e13a5c9fc5
Szymon <ctk7000@gmail.com>
2021-03-27 20:30:01 +0100

    getting python path automatically

--- src/TikTokWrapper/TikTokWrapper.Core/Factories/TikTokManagerFactory.cs
+++ src/TikTokWrapper/TikTokWrapper.Core/Factories/TikTokManagerFactory.cs
@@ -1,5 +1,6 @@
 ﻿using TikTokWrapper.Core.Internal;
 using TikTokWrapper.Core.Internal.Utilities;
+using TikTokWrapper.Core.Providers;
 
 namespace TikTokWrapper.Core.Factories
 {
@@ -7,6 +8,14 @@ namespace TikTokWrapper.Core.Factories
     {
         public ITikTokManager Create(string pythonExePath)
         {
+            var pythonRunner = new PythonRunner(pythonExePath);
+            return new TikTokManager(pythonRunner);
+        }
+
+        public ITikTokManager Create()
+        {
+            var pythonExePathProvider = new PythonExecutablePathProvider();
+            var pythonExePath = pythonExePathProvider.Get();
             var pythonRunner = new PythonRunner(pythonExePath);
             return new TikTokManager(pythonRunner);
         }
