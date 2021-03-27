859bc514524178284e3ed69359dd4db57eaef8d4
Szymon <ctk7000@gmail.com>
2021-03-27 20:45:27 +0100

    added test

---
+++ src/TikTokWrapper/TikToWrapper.FunctionalTests/TrendingTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TikTokWrapper.Core;
+using TikTokWrapper.Core.Factories;
+
+namespace TikToWrapper.FunctionalTests
+{
+    [TestFixture]
+    public class TrendingTests
+    {
+        private ITikTokManager _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var factory = new TikTokManagerFactory();
+            _sut = factory.Create();
+        }
+
+        [Test]
+        public void ShouldGetTrendingData()
+        {
+            // Arrange
+            var expectedNumberOfTikToks = 10;
+
+            // Act
+            var actual = _sut.GetTrending();
+
+            // Assert
+            actual.Count.Should().Be(expectedNumberOfTikToks);
+        }
+        
+    }
+}
